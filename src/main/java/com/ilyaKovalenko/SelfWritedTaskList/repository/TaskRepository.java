8a29f456a87b95d4f6794b14f504211666810e06
1lya_kovalenko <kovalenkoilyaden@gmail.com>
2024-11-17 12:14:25 +0300

    Commit after adding redis service in docker. Docker container in general still need to be adding

--- src/main/java/com/ilyaKovalenko/SelfWritedTaskList/repository/TaskRepository.java
+++ src/main/java/com/ilyaKovalenko/SelfWritedTaskList/repository/TaskRepository.java
@@ -2,6 +2,7 @@ package com.ilyaKovalenko.SelfWritedTaskList.repository;
 
 import com.ilyaKovalenko.SelfWritedTaskList.domain.Task.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
